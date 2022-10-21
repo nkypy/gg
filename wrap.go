3a1f946c2feaaaa99dd21721361a2aea28060320
Jack Shih <develop@kshih.com>
2022-10-21 19:52:25 +0800

    support space

--- wrap.go
+++ wrap.go
@@ -2,27 +2,10 @@ package gg
 
 import (
 	"strings"
-	"unicode"
 )
 
 type measureStringer interface {
 	MeasureString(s string) (w, h float64)
-}
-
-func splitOnSpace(x string) []string {
-	var result []string
-	pi := 0
-	ps := false
-	for i, c := range x {
-		s := unicode.IsSpace(c)
-		if s != ps && i > 0 {
-			result = append(result, x[pi:i])
-			pi = i
-		}
-		ps = s
-	}
-	result = append(result, x[pi:])
-	return result
 }
 
 func splitRune(x string) []string {
@@ -37,12 +20,8 @@ func wordWrap(m measureStringer, s string, width float64) []string {
 	var result []string
 	for _, line := range strings.Split(s, "\n") {
 		fields := splitRune(line)
-		// fields := splitOnSpace(line)
-		if len(fields)%2 == 1 {
-			fields = append(fields, "")
-		}
 		x := ""
-		for i := 0; i < len(fields); i += 2 {
+		for i := 0; i < len(fields); i++ {
 			w, _ := m.MeasureString(x + fields[i])
 			if w > width {
 				if x == "" {
@@ -54,14 +33,11 @@ func wordWrap(m measureStringer, s string, width float64) []string {
 					x = ""
 				}
 			}
-			x += fields[i] + fields[i+1]
+			x += fields[i]
 		}
 		if x != "" {
 			result = append(result, x)
 		}
-	}
-	for i, line := range result {
-		result[i] = strings.TrimRight(line, " ")
 	}
 	return result
 }
